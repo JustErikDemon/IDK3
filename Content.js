31f8979c951efb8d21dca9000c28dbd447a0633f
JustErikDemon <160011349+JustErikDemon@users.noreply.github.com>
2024-10-26 08:21:53 +0200

    Update and rename popup.js to Content.js

---
+++ Content.js
@@ -0,0 +1,7 @@
+// Content.js
+
+// Example functionality: Modify the page or log a message when visiting YouTube
+console.log("BobRossRocks Extension Loaded!");
+
+// Inject a message or change an element on the page as an example
+document.body.style.border = "5px solid lightblue";
